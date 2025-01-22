9bc82f064ec42688600b026a99b6b68a4d77593e
Taimour S. <market.advertisment@gmail.com>
2025-01-22 22:52:21 +0500

    Update Time Left Function.js
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

--- Time Left Function.js
+++ Time Left Function.js
@@ -1,7 +1,7 @@
 function lifeInWeeks(age) {
     var lifeEnd = 90;
-    var yearsLeft= 90-age;
-    console.log("If your age is "+age+", then you have just"+(yearsLeft*365)+"days, "+(yearsLeft*52.1429)+"weeks & "+(yearsLeft*12)+" months left" );
+    var yearsLeft = 90 - age;
+    console.log("If your age is " + age + ", then you have just " + Math.floor(yearsLeft * 365) + " days, " + Math.floor(yearsLeft * 52.1429) + " weeks & " + Math.floor(yearsLeft * 12) + " months left");
 }
 
 lifeInWeeks(56);
