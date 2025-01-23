84c025f7db3f0b14fdb0953e9f1818c564b7f173
Taimour S. <market.advertisment@gmail.com>
2025-01-23 12:06:08 +0500

    Update BMI Formula.js
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

--- BMI Formula.js
+++ BMI Formula.js
@@ -2,8 +2,11 @@
 
 function bmi(weight, height) {
     var w = weight;
-    var h = height;
-    console.log("If you height is "+h+"m & your weight = "+w+"kg then your BMI = "+w/h+"Kg/m^2");
+    var hh = Math.pow(height,2);
+    console.log("If you height is "+height+"m & your weight = "+w+"kg then your BMI = "+w/hh+" Kg/m^2");
+        console.log("Age: 18–34 | Ideal BMI = 23.0–25.9");
+        console.log("Age: 35–44 | Ideal BMI = 23.0–26.9");
+        console.log("Age: 45–54	| Ideal BMI = 24.0–27.9");
 }
 
-bmi(50,2);
+bmi(53,1.64);
