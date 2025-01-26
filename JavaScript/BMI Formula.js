816546484c1408aa2163e01a910dbaae4c2701df
Taimour S. <market.advertisment@gmail.com>
2025-01-26 17:17:29 +0500

    Advance BMI Formula Program.js
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

--- JavaScript/BMI Formula.js
+++ JavaScript/BMI Formula.js
@@ -1,12 +1,18 @@
-// bmi = weight / height
+function bmiCalculator() {
+    var weight = prompt("Enter your weight (in kg):");
+    var height = prompt("Enter your height (in meters):");
 
-function bmi(weight, height) {
-    var w = weight;
-    var hh = Math.pow(height,2);
-    console.log("If you height is "+height+"m & your weight = "+w+"kg then your BMI = "+w/hh+" Kg/m^2");
-        console.log("Age: 18–34 | Ideal BMI = 23.0–25.9");
-        console.log("Age: 35–44 | Ideal BMI = 23.0–26.9");
-        console.log("Age: 45–54	| Ideal BMI = 24.0–27.9");
+    var hh = Math.pow(height, 2);
+    var result = weight / hh;  
+
+    if (result < 18.5) {
+        alert("Your BMI is " + result.toFixed(2) + ", so you are underweight.");
+    } else if (result <= 24.9) {
+        alert("Your BMI is " + result.toFixed(2) + ", so you have a normal weight.");
+    } else {
+        alert("Your BMI is " + result.toFixed(2) + ", so you are overweight.");
+    }
 }
 
-bmi(53,1.64);
+var BMI = bmiCalculator(53, 1.64);
+BMI; // Returns the message as output
