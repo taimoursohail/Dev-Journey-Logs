812c7a6634e2f8fb46d5ce9ca59be727b6232507
Taimour S. <market.advertisment@gmail.com>
2025-01-26 12:48:46 +0500

    Delete JavaScript directory
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

--- JavaScript/Time Left Function.js
+++
@@ -1,7 +0,0 @@
-function lifeInWeeks(age) {
-    var lifeEnd = 90;
-    var yearsLeft = 90 - age;
-    console.log("If your age is " + age + ", then you have just " + Math.floor(yearsLeft * 365) + " days, " + Math.floor(yearsLeft * 52.1429) + " weeks & " + Math.floor(yearsLeft * 12) + " months left");
-}
-
-lifeInWeeks(56);
