3852a744c61277825d6348093985e78b790c7f42
Taimour S. <market.advertisment@gmail.com>
2025-02-02 15:22:39 +0500

    Add files via upload
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

---
+++ JavaScript/Randomly Select Array Items using proeprties.js
@@ -0,0 +1,11 @@
+function whosPaying(names) {
+  
+    var x = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+    
+    var y = Math.floor(Math.random() * x.length); 
+    
+    var z = x[y]; 
+    
+    return z + " is going to buy lunch today!";
+    
+}
