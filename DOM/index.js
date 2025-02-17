608498a9c9aff68defa5bba6786a3c5f4dc7cfb3
Taimour S. <market.advertisment@gmail.com>
2025-02-17 12:04:15 +0500

    Add files via upload
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

---
+++ DOM/index.js
@@ -0,0 +1,4 @@
+var heading = document.firstElementChild.lastElementChild.firstElementChild;
+heading.innerHTML = "I have Manupulated the H1 tag";
+heading.style.color = "red";
+document.firstElementChild.lastElementChild.querySelector(".country").children[4].innerHTML = "Bulgaria (Manupulated) ";
