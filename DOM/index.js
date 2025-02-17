be99d15bef2de3fe57778f3bb15bb753a8cf49af
Taimour S. <market.advertisment@gmail.com>
2025-02-17 12:05:07 +0500

    Delete DOM/index.js
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

--- DOM/index.js
+++
@@ -1,4 +0,0 @@
-var heading = document.firstElementChild.lastElementChild.firstElementChild;
-heading.innerHTML = "I have Manupulated the H1 tag";
-heading.style.color = "red";
-document.firstElementChild.lastElementChild.querySelector(".country").children[4].innerHTML = "Bulgaria (Manupulated) ";
