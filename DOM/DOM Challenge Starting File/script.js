19f23fed1ac637518bc0876fa2b9f0d7e4e5ff90
Taimour S. <market.advertisment@gmail.com>
2025-02-23 11:30:15 +0500

    Uploaded 3 core files of DOM
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

---
+++ DOM/DOM Challenge Starting File/script.js
@@ -0,0 +1,37 @@
+document.getElementsByClassName("list")[0].innerHTML = 
+    '<a href="https://www.expedia.com">London</a>';
+
+document.getElementsByTagName("li")[1].innerHTML = 
+    "Nottingham";
+
+document.getElementById("Third").innerHTML = 
+    "Walsall";
+
+document.querySelectorAll(".list")[3].innerHTML = 
+    "Manchester";
+
+document.querySelectorAll("ul .list")[4].innerHTML = 
+    "Birmingham";
+
+document.querySelectorAll("ul .list")[4].innerHTML = 
+    "Wolverhempton";
+
+document.querySelectorAll("body ul .list")[5].innerHTML = 
+    "Dudly";
+
+document.querySelectorAll(".list")[5].style.color = 
+    "Green"; //by this you can't change the entire class elements because .style (setter property only applicable on single element because getElementsByClassName() returns a collection)
+
+document.querySelector("input").checked = true;
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").style.color = "#ffd700";
+
+document.querySelector("h1").innerHTML = "<u>'Hello World'</u>";
+
+document.querySelector("h1").style.fontSize = "15rem";
+
+document.querySelector("h1").classList.add("huge"); // I've added that class into external css file and re-sized the font
+
+// TEXT Manupulation innerHTML v/s innerContent vs innerText
