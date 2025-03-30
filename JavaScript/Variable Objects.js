076d8e5f9004156c1e2e38730f8571c8b29a8e6f
Taimour S. <market.advertisment@gmail.com>
2025-03-31 04:47:55 +0500

    Add files via upload
    Signed-off-by: Taimour S. <market.advertisment@gmail.com>

---
+++ JavaScript/Variable Objects.js
@@ -0,0 +1,13 @@
+var houseKeeper = {
+
+    name: "Zahid",
+    age: "32",
+    hasWorkPermit: "Yes",
+    experience: 15 + " Years +",
+    hasOwnCar: "Yes",
+    expertise: ["Laundry", "Dusting", "Kidnapping 😊", "Stealing"],
+    
+
+}
+
+houseKeeper.name;
